0a415d1f6a7b34f96849813e35406b6e11a4777a
mari <iwannabemari@gmail.com>
2019-03-02 12:09:34 +0100

    simplified gerhard component

--- src/components/Gerhard.vue
+++ src/components/Gerhard.vue
@@ -3,42 +3,10 @@
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
         <quote>HAHAHA I BIMS 1 GERHARD!</quote>
-        <v-btn icon @click="navigate()">
+        <v-btn icon @click="()=>{this.$router.push({name:'Invoices'});}">
           <img src="@/assets/gerhard.jpg" contain height="800">
         </v-btn>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
-
-<script>
-  export default {
-    name: 'NotFound',
-    methods: {
-      navigate() {
-        this.$router.go(-1);
-      }
-    }
-  }
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
-</style>
